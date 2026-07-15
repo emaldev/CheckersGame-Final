efe481d84437bddbcc4924a84bc2572dc5d23147
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-15 06:58:32 +0330

    feat: implement Board class with Tile matrix initialization

--- Board.java
+++ Board.java
@@ -1,3 +1,41 @@
+// این کلاس نماینده اصلی بازی است که شامل سطر و ستون ها است 
+// در این کلاس 64 خانه از tile ساخته می شود 
 public class Board {
+
+    // آرایه دو بعدی 
+    private Tile[][] board;
+
+    // سازنده 
+    public Board(){
+        // ایجاد صفحه با ابعاد 8 در 8 
+        board = new Tile[8][8];
+        // مقدار دهی کردن تمام خانه ها 
+        createBoard();
+
+
+    }
+
+    // ساخت صفحه بازی رنگ و موقیعت خانه 
+    private void createBoard(){
+        
+        for(int row = 0; row < 8; row++){
+            for(int col = 0; col < 8; col++){
+                // اگر جمع سطرو ستون فرد باشد خانه تیره است 
+                boolean dark = (row + col) % 2 != 0;
+                 // ساخت خانه  
+                board[row][col] = new Tile(dark, row, col);
+            }
+        }
+    }
+
+    //برگرداندن کل صفحه بازی 
+    public Tile[][] getBoard(){
+        return board;
+    }
+    //برگرداندن یک خانه مشحض 
+    public Tile getTile(int row, int col){
+        return board[row][col];
+    }
+    
     
 }
