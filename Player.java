d2d361e7181cc61291ca078dd62128af901fc8c4
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-15 06:40:56 +0330

    Adding methods to the Player class.

--- Player.java
+++ Player.java
@@ -1,3 +1,23 @@
+// این کلاس نماینده بازی کنان در بازی است 
 public class Player {
+    
+     // شناسه بازیکن 
+    private String id;
+
+    // سازنده 
+    public Player(String id){
+        this.id = id;
+    }
+
+    // دریافت شناسه بازیکن 
+    public String getId(){
+        return id;
+    }
+
+    // بغییر شناسه بازیکن 
+    public void setId(String id){
+        this.id = id;
+    }
+
     
 }
