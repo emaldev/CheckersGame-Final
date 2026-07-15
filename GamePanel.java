fe326d89d6af44818134164b610651cbf01d7cac
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-15 08:30:03 +0330

    Add GamePanel base structure

--- GamePanel.java
+++ GamePanel.java
@@ -1,4 +1,27 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
 
-public class GamePanel {
+// این کلاس مسول رسم مهره ها و نمایش آن ها است 
+public class GamePanel extends JPanel {
+    
+    private Game game;
+    
+    // سازنده 
+    public GamePanel(Game game){
+        this.game = game;
+    }
+
+    // رسم صفحه بازی 
+    @Override 
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+    }
+         // اندازه پنل بازی 
+    @Override 
+    public java.awt.Dimension getPreferredSize(){
+        return new java.awt.Dimension(640, 640);
+    }
+
   
 }
