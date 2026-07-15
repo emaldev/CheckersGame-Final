8243bd36456263e719be05cf625910a22d89be59
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-15 07:26:32 +0330

    feat: create GameFrame and connect it to Game class

--- GameFrame.java
+++ GameFrame.java
@@ -1,3 +1,20 @@
-public class GameFrame {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GameFrame extends JFrame{
+
+    // سازنده 
+    public GameFrame(Game game){
+        this.setTitle("Checkers Game");
+        // اضافه کردن صفحه بازی 
+        this.add(new GamePanel());// اتصال به گیم پنل 
+         // تنظیم اندازه پنجره 
+        this.pack();
+        // مکان قرار گرفتن پنجره 
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setVisible(true);
+    }
     
 }
