6ad015d26a0483232a574b8441192e8f305fa666
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-15 07:14:38 +0330

    feat: implement Game class with player and board management.

--- Game.java
+++ Game.java
@@ -1,3 +1,43 @@
+// مدیریت کل بازی 
 public class Game {
+
+    // صفحه بازی 
+    private Board board;
+     // آرایه بازی کنان 
+    private Player[] players;
+    // بازی کنی که نوبت اول از او است 
+    private Player currentPlayer;
+
+    // سازنده 
+    public Game(){
+        // ساخت صفحه بازی 
+    board = new Board();
+    // ساخت دو بازیکن 
+    players = new Player[2];
+    players[0] = new Player("Player 1");
+    players[1] = new Player("Player 2");
+
+    // بازی با بازیکن اول شروع می شود 
+    currentPlayer = players[0];
+
     
+    
+    }
+
+    // برگرداندن صفحه بازی 
+    public Board getBoard(){
+        return board;
+    }
+    // برگداندن بازیکنی که نوبت از او است 
+    public Player getCurrentPlayer(){
+        return currentPlayer;
+    }
+    // تغییر نوبت بازیکنان 
+    public void changeTurn(){
+        if(currentPlayer == players[0] ) {
+            currentPlayer = players[1]; 
+        }else{
+            currentPlayer = players[0];
+        }
+    }
 }
