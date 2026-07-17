2d9a0cdb93456cf70c4b164d4222df68952da0a8
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-17 13:35:36 +0330

    Completion of the method of board

--- Board.java
+++ Board.java
@@ -153,6 +153,36 @@ if(piece == 4){
         return true;
     }
 }
+// خوردن با شاه بازیکن اول
+if(piece == 3){
+
+    if(Math.abs(toRow - fromRow) == 2 &&
+       Math.abs(toCol - fromCol) == 2){
+
+        int middleRow = (fromRow + toRow) / 2;
+        int middleCol = (fromCol + toCol) / 2;
+
+        if(board[middleRow][middleCol].getStatus() == 2){
+            return true;
+        }
+    }
+}
+
+
+// خوردن با شاه بازیکن دوم
+if(piece == 4){
+
+    if(Math.abs(toRow - fromRow) == 2 &&
+       Math.abs(toCol - fromCol) == 2){
+
+        int middleRow = (fromRow + toRow) / 2;
+        int middleCol = (fromCol + toCol) / 2;
+
+        if(board[middleRow][middleCol].getStatus() == 1){
+            return true;
+        }
+    }
+}
 
 
     return false;
@@ -186,6 +216,26 @@ if(piece == 2 && toRow == 0){
     board[toRow][toCol].setStatus(4);
 }
 
+}
+// بررسی وجود مهره های بازیکن
+public boolean hasPieces(int player){
+
+    for(int row = 0; row < 8; row++){
+        for(int col = 0; col < 8; col++){
+
+            int piece = board[row][col].getStatus();
+
+            if(player == 1 && (piece == 1 || piece == 3)){
+                return true;
+            }
+
+            if(player == 2 && (piece == 2 || piece == 4)){
+                return true;
+            }
+        }
+    }
+
+    return false;
 }
     
     
